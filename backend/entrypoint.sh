5e2bc7b7152eadb74e393ecf72e5a60637041cfd
Zachary McSween Manickchand <zacharymcsweenmanickchand@gmail.com>
2025-10-08 20:56:59 -0400

    Base de donné fonctionelle

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,7 +6,5 @@ python manage.py makemigrations
 # Migrer application
 python manage.py migrate
 
-python3 manage.py runserver 0.0.0.0:8000
-
 # Application accessible sur http://localhost:8000
-#python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
