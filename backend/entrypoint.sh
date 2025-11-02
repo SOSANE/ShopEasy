9e6ed6e525dd3d6a8300455824f7c1a5d98e0294
nelie-mabelle <noumbo.nelie@yahoo.fr>
2025-11-02 15:59:13 -0500

    Ajout du formulaire amelioration de paiement

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,12 +4,7 @@
 python manage.py makemigrations
 
 # Migrer application
-python manage.py migrate auth
-python manage.py migrate admin
-python manage.py migrate authtoken
-python manage.py migrate contenttypes
-python manage.py migrate sessions
-python manage.py migrate backend_models
+python manage.py migrate
 
 # Application accessible sur http://localhost:8000
 python manage.py runserver 0.0.0.0:8000
