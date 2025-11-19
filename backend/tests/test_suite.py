94b7750d9f38b5109574592705e3198c872a6092
SOSANE <sosanemh.pro@gmail.com>
2025-11-18 20:06:48 -0500

    #81 - format

--- backend/tests/test_suite.py
+++ backend/tests/test_suite.py
@@ -1,9 +1,6 @@
-
-
 from tests.api.test_commande import CommandeTest
 from tests.api.test_produit import ProduitTest
 from tests.api.test_produit_panier import ProduitPanierTest
-
 
 
 # Insertion des tests de la commande
